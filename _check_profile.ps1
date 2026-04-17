905a0151bc383dc6b3d73cc17ecb5948934b6e1d
Vadim <vadim@gsadus.com>
2026-04-17 10:44:51 -0700

    wip: 20260417-1044 [skip ci]

--- _check_profile.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host ('PROFILE path: ' + $PROFILE)
-Write-Host ('Exists: ' + (Test-Path $PROFILE))
-if (Test-Path $PROFILE) {
-    Get-Content $PROFILE | Select-Object -First 20
-}
-Write-Host '--- available wip functions ---'
-Get-Command *wip* -ErrorAction SilentlyContinue | Format-Table Name, CommandType
